95cd79a1ce75656a714f6c81b1c303db9b01bc07
kassa <mr.ethioskassa@gmail.com>
2023-02-16 15:03:57 -0800

    my first ever C code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	/* Strings Begin */
+	char str[] = "Programming is like building a multilingual puzzle";
+	
+	/* Now print on OUT */
+	puts(str);
+
+	return 0;
+}
