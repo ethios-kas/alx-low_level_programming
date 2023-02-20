94a74d1f89cb41379d995780ffebb08fcd7f18ef
kassa <mr.ethioskassa@gmail.com>
2023-02-20 08:05:14 -0800

    lets get the base 16 digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Start of the program
+ * Return: success bring me the money
+ */
+
+int main(void)
+{
+	int n, a;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+
+	for (a = 'a'; a <= 'f'; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
