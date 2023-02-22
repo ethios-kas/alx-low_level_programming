f732bafb0c745d5a1835981a035634d147cab4be
kassa <mr.ethioskassa@gmail.com>
2023-02-21 16:44:04 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10- check the code.
  *
  * Return: Always 0.
  */
-int main(void)
+
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
+	char c;
+	int = 0;
+
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
 }
