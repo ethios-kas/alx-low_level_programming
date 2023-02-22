534734e8efa6bfae44cc0a42ae483ff5a7f0dd87
kassa <mr.ethioskassa@gmail.com>
2023-02-21 16:59:06 -0800

    dfdf

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * -islower - check for lower case
+ *  @c: return charach=ter check
+ *  Return: 1 ro 0
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
