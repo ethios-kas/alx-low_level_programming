f00acc4b56415d68ec1626af1df0ec662b4d9ca8
kassa <mr.ethioskassa@gmail.com>
2023-02-21 17:07:29 -0800

    df

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpah - checks for alphabet
+ * @c: the char tp be checked
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 98) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
