650d1bee4f0750e08d53e85ac9249642a81fa144
kassa <mr.ethioskassa@gmail.com>
2023-02-23 12:07:47 -0800

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - multiplies a and b
+ *@a: calls the first int
+ *@b: calls 2nd int
+ *Return: results the two
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
