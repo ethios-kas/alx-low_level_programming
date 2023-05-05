3431d1f69decc21f0dec657fd4117d3c9ad5c764
Bereket Aynalem <mr.ethioskassa@gmail.com>
2023-05-05 15:46:43 -0700

    print binary from base 10)

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binary of a number
+ * @n: integer
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	int tzero = 0, i;
+	unsigned long int x;
+
+	for (i = 23; i >= 0; i--)
+	{
+		x = (n >> i) & 1;
+		if (x == 1)
+			tzero = 1;
+		if (tzero == 1)
+			_putchar((x & 1) + '0');
+	}
+	if (n == 0)
+		_putchar('0');
+}
