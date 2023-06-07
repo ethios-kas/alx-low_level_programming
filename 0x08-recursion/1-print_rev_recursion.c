090bc7ea1736ce23222d6a685cdc2fb881a95ac3
kassa <mr.ethioskassa@gmail.com>
2023-06-07 10:39:13 -0700

    Recursion100

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _print_rev_recursion -  string in reverse
+  * @s: its a string to reverse
+  *
+  * Return: success.
+  */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
